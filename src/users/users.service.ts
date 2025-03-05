0e3f399d83ed3729393c737de70a5f332e692259
thailqvvt <thailq@vvt.com.vn>
2025-03-05 15:10:28 +0700

    CRU

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UsersService {
-  private users: string[] = [];
+  private users: any[] = [];
 
   findAll() {
     return this.users;
+  }
+
+  create({ name }) {
+    const newUser = {
+      id: uuidv4(), // Tạo ID khi thêm dữ liệu mới
+      name,
+    };
+    this.users.push(newUser);
+    return { message: 'User created', name };
+  }
+
+  edit({ name, id }) {
+    let temp: { id: string; name: string }[] = [...this.users];
+    const idx = temp.findIndex((e) => e.id === id);
+    temp[idx] = { id, name };
+    this.users = temp;
+    return { message: 'User update', id };
   }
 }
